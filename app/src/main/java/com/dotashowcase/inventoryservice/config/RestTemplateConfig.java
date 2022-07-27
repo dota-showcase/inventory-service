35f67cba603cdfa917a0bf404653463d9b14bc83
EXayer <itwasntluck@gmail.com>
2022-07-27 03:41:44 +0300

    Config for http client.

---
+++ app/src/main/java/com/dotashowcase/inventoryservice/config/RestTemplateConfig.java
@@ -0,0 +1,42 @@
+package com.dotashowcase.inventoryservice.config;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    final CloseableHttpClient httpClient;
+
+    public RestTemplateConfig(CloseableHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+       return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(httpClient);
+
+        return clientHttpRequestFactory;
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+
+        scheduler.setThreadNamePrefix("poolScheduler");
+        scheduler.setPoolSize(50);
+
+        return scheduler;
+    }
+}
